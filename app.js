f76fdbbd0ede70789c4a3b133cbb0694d92936ae
Needleless1745 <josemsanchez8@gmail.com>
2026-03-26 21:09:13 -0500

    user controllers implemented&tested

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const indexRouter = require("./routes/index");
 
 const app = express();
-
 const { PORT = 3001 } = process.env;
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch(console.error);
+
+app.use(express.json());
+app.use("/", indexRouter);
 
 app.listen(PORT, () => {
   console.log(`This thing on? Port: ${PORT}`);
