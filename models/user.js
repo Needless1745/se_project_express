23e7e7e41891f145f66450dd8c67d8234718a7df
Needleless1745 <josemsanchez8@gmail.com>
2026-05-14 00:19:26 -0500

    User Schema updated

--- models/user.js
+++ models/user.js
@@ -2,6 +2,22 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator(value) {
+        return validator.isEmail(value);
+      },
+      message: "You must enter a valid email address",
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+  },
   name: {
     type: String,
     required: true,
