ca67caeb84beaccc3c785d80348c599477e835d2
Needleless1745 <josemsanchez8@gmail.com>
2026-06-14 21:25:58 -0500

    secret key reconfig

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const { requestLogger, errorLogger } = require("./middlewares/logger");
 const routes = require("./routes");
 const errorHandler = require("./middlewares/error-handler");
+require("dotenv").config();
 
 const app = express();
 const { PORT = 3001 } = process.env;
